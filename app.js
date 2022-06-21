9c73c31480082550b358241216fb5bb589b74a0d
sombrerodiego <sombrerodiego@gmail.com>
2022-06-21 18:13:52 -0300

    Cambie App

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-const express = require('express');
-const path = require(path);
-const app = express();
+//console.log("Hola Mundo!");
 
-//MIDDLEWARE
-app.use(express.static('public'));
-//Otra forma - en Playground
-//app.set('puerto', process.env.PORT !! 3000);
-
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
-
-// -------RUTAS-------
-
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
-
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
-
-app.listen(3000, ()=>{
-    console.log(`Server running at http://${HOST}:${PORT}/`);
-});
-
-//OTRA FORMA - en Playground
-/*
-app.listen(app.get('puerto'), ()=> console.log( "Servidor") $(app.get('puerto')))
-app.listen(process.env.PORT || 3000, function()
-{ console.log()}*/
+let name = "Juan";
