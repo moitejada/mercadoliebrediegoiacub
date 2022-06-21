49c599aa1420a299460f902024e10e16c8528a9b
sombrerodiego <sombrerodiego@gmail.com>
2022-06-21 20:31:30 -0300

    VA3

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const express = require('express');
-const path = require(path);
 const app = express();
-
-//MIDDLEWARE
 app.use(express.static('public'));
-//Otra forma - en Playground
-//app.set('puerto', process.env.PORT !! 3000);
-
-//app.use(express.static(path.resolve(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
-
-// -------RUTAS-------
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
@@ -26,14 +17,6 @@ app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
 
-//app.listen(3000, ()=> { console.log('listening on http://localhost:3000')})
-
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log(`Server running at http://${HOST}:${PORT}/`);
 });
-
-//OTRA FORMA - en Playground
-/*
-app.listen(app.get('puerto'), ()=> console.log( "Servidor") $(app.get('puerto')))
-app.listen(process.env.PORT || 3000, function()
-{ console.log()}*/
