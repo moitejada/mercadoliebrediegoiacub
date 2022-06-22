29938f49fa18b6d1977a64d40e762c7fbf834288
sombrerodiego <sombrerodiego@gmail.com>
2022-06-22 16:44:31 -0300

    Acomodando Style

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.resolve(__dirname, 'public'))
 );
@@ -10,14 +10,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
-app.listen(PORT, () => {
-    console.log('listening on http://localhost:3000');
-});
-
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
 app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('listening on http://localhost:3001');
 });
