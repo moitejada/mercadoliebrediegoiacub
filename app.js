f0eaed17cb53f57a63272daf0a84686e7de45f80
sombrerodiego <sombrerodiego@gmail.com>
2022-06-22 15:44:34 -0300

    Ultimas Modificaciones

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 4000;
-const HOST = process.env.HOST || 'localhost';
+app.use(express.static(path.resolve(__dirname, 'public'))
+);
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+app.listen(PORT, () => {
+    console.log('listening on http://localhost:3000');
 });
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server running at http://${HOST}:${PORT}/`);
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
